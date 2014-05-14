cb42fc51b5782bae0ff909a99594d06a5b85d28b
Martin Skargeth <martin@skargeth.at>
2014-05-14 17:40:03 +0200

    [MS] {other} - Improved animation a little bit.

--- core/src/com/reu/game/TestObject.java
+++ core/src/com/reu/game/TestObject.java
@@ -22,6 +22,8 @@ public class TestObject extends Actor{
 	 TextureRegion      currentFrame;
 	 
 	 float stateTime;
+	 float stopTime;
+	 boolean animate;
 	 
 	 TestObject()
 	 {
@@ -41,13 +43,15 @@ public class TestObject extends Actor{
 	        	 walkFrames[index++] = tmp[i][j];
 	         }
 	     }
-	     walkAnimation = new Animation(0.025f, walkFrames);      // #11
-	     spriteBatch = new SpriteBatch();                // #12
-	     stateTime = 0f;                         // #13
+	     walkAnimation = new Animation(0.025f, walkFrames);
+	     spriteBatch = new SpriteBatch();
+	     stateTime = 0f;
+	     animate = true;
 	 }
 	 
 	 public void MoveTo(float x, float y)
 	 {
+		 animate = true;
 		 clearActions();
 		 Vector2 current_position = new Vector2(getX(), getY());
 		 Vector2 new_position = new Vector2(x, y);
@@ -83,16 +87,26 @@ public class TestObject extends Actor{
 		 float rotation_time = 1f / 360f * Math.abs(old_angle - angle);
 		 float moving_time = length / 500f;
 		 addAction(sequence(rotateTo(angle, rotation_time), moveTo(x,y, moving_time)));
+		 stopTime = stateTime + rotation_time + moving_time;
 	 }
 	 
 	 @Override
 	 public void draw(Batch batch, float parentAlpha)
 	 {
-		 stateTime += Gdx.graphics.getDeltaTime();           // #15
-	     currentFrame = walkAnimation.getKeyFrame(stateTime, true);  // #16
+		 stateTime += Gdx.graphics.getDeltaTime();
+		 if(animate)
+		 {
+			 currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+			 if(stateTime > stopTime)
+				 animate = false;
+		 }
+		 else
+		 {
+			 currentFrame = walkAnimation.getKeyFrame(0.5f, true);
+		 }
 	     spriteBatch.begin();
 	     spriteBatch.draw(currentFrame, getX(), getY(), getOriginX(), getOriginY(),
-	    	     getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());             // #17
+	    	     getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
 	     spriteBatch.end();
 	 }
 	 
