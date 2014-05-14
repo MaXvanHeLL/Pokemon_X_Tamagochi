82566467ef77c03e226ed9b9fddb9a77622d9e58
Martin Skargeth <martin@skargeth.at>
2014-05-14 17:26:40 +0200

    [MS, DK] {other} - Added firs class and played arround with scenes. Created Images.

--- core/src/com/reu/game/ReuGame.java
+++ core/src/com/reu/game/ReuGame.java
@@ -2,34 +2,114 @@ package com.reu.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateByAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class ReuGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	float red = 0;
-	float green = 0;
+	Stage stage;
+	Skin skin;
+	Texture tex;
+	TestObject test;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage();
+		
+		Gdx.input.setInputProcessor(stage);
+		// A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
+		// recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
+		Skin skin = new Skin();
+
+		// Generate a 1x1 white texture and store it in the skin named "white".
+		Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
+		pixmap.setColor(Color.WHITE);
+		pixmap.fill();
+		skin.add("white", new Texture(pixmap));
+		// Store the default libgdx font under the name "default".
+		skin.add("default", new BitmapFont());
+		// Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+		textButtonStyle.font = skin.getFont("default");
+		skin.add("default", textButtonStyle);
+		skin.add("room", new Texture(Gdx.files.internal("house.png")));
+		
+		// Create a table that fills the screen. Everything else will go inside this table.
+		Table table = new Table();
+		table.setBackground(skin.getDrawable("room"));
+		table.setFillParent(true);
+		table.align(Align.top | Align.left);
+		stage.addActor(table);
+		
+		// Add dummy object to table... else screen would be empty
+		table.add(new Image(skin.getDrawable("white")));
+
+		
+		/*// Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+		final TextButton button = new TextButton("Click me!", skin);
+		table.add(button).width(200);
+
+		// Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
+		// Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
+		// ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
+		// revert the checked state.
+		button.addListener(new ChangeListener() {
+		public void changed (ChangeEvent event, Actor actor) {
+		System.out.println("Clicked! Is checked: " + button.isChecked());
+		button.setText("Good job!");
+		}
+		});*/
+		
+		// Hier wird "Knuddel" implementiert!
+		test = new TestObject();
+		stage.addActor(test);
+		// Mache die ganze stage "klickbar" - wir sollten über Wege nachdenken
+		stage.addListener(new InputListener(){
+			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+		        test.MoveTo(x, y);
+		        return true;
+		}});
 	}
 
 	@Override
+	public void resize (int width, int height) {
+	    // See below for what true means.
+	    stage.getViewport().update(width, height, true);
+	}
+	
+	@Override
 	public void render () {
-		red += 0.01;
-		green += 0.001;
-		Gdx.gl.glClearColor(red, green, 0, 1);
+
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, (int)(red * 1024.0), (int)(green * 800.0));
-		batch.end();
-		if(red > 1)
-			red = 0;
-		if(green > 1)
-			green = 0;
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+		
+		Table.drawDebug(stage);
 	}
 }
---
+++ core/src/com/reu/game/TestObject.java
@@ -0,0 +1,100 @@
+package com.reu.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
+
+public class TestObject extends Actor{
+	 private static final int    FRAME_COLS = 1;
+	 private static final int    FRAME_ROWS = 16; 
+	 
+	 Animation 			walkAnimation;
+	 Texture            walkSheet;
+	 TextureRegion[]    walkFrames;
+	 SpriteBatch        spriteBatch;
+	 TextureRegion      currentFrame;
+	 
+	 float stateTime;
+	 
+	 TestObject()
+	 {
+		 setPosition(Gdx.graphics.getWidth()/2.0f, Gdx.graphics.getHeight()/2.0f);
+		 setWidth(128);
+		 setHeight(128);
+		 setOrigin(64, 64);
+		 setScale(0.8f);
+		 walkSheet = new Texture(Gdx.files.internal("sprites.png"));
+	     TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/FRAME_COLS, walkSheet.getHeight()/FRAME_ROWS);
+	     walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+	     int index = 0;
+	     for (int i = 0; i < FRAME_ROWS; i++) 
+	     {
+	         for (int j = 0; j < FRAME_COLS; j++) 
+	         {
+	        	 walkFrames[index++] = tmp[i][j];
+	         }
+	     }
+	     walkAnimation = new Animation(0.025f, walkFrames);      // #11
+	     spriteBatch = new SpriteBatch();                // #12
+	     stateTime = 0f;                         // #13
+	 }
+	 
+	 public void MoveTo(float x, float y)
+	 {
+		 clearActions();
+		 Vector2 current_position = new Vector2(getX(), getY());
+		 Vector2 new_position = new Vector2(x, y);
+		 Vector2 distance = new_position.sub(current_position);
+		 float length = distance.len();
+		 
+		 float angle = distance.angle() - 90;
+		 if(angle < 0)
+		 {
+			 angle += 360;
+		 }
+		 if(getRotation() > 360)
+		 {
+			 setRotation(getRotation()-360);
+		 }
+		 else if(getRotation() < 0)
+		 {
+			 setRotation(getRotation()+360);
+		 }
+		 float old_angle = getRotation();
+		 
+		 
+		 if(old_angle > 180 && angle < 180 && (old_angle - angle) > 180)
+		 {
+			 angle += 360; 
+		 }
+		 else if(old_angle < 180 && angle > 180 && (angle - old_angle) > 180)
+		 {
+			 angle -= 360;
+		 }
+
+		 
+		 float rotation_time = 1f / 360f * Math.abs(old_angle - angle);
+		 float moving_time = length / 500f;
+		 addAction(sequence(rotateTo(angle, rotation_time), moveTo(x,y, moving_time)));
+	 }
+	 
+	 @Override
+	 public void draw(Batch batch, float parentAlpha)
+	 {
+		 stateTime += Gdx.graphics.getDeltaTime();           // #15
+	     currentFrame = walkAnimation.getKeyFrame(stateTime, true);  // #16
+	     spriteBatch.begin();
+	     spriteBatch.draw(currentFrame, getX(), getY(), getOriginX(), getOriginY(),
+	    	     getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());             // #17
+	     spriteBatch.end();
+	 }
+	 
+
+}
