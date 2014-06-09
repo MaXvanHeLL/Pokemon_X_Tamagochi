84bd5cdf51b78c47e4db8cb17ff197cee8de884e
Markus Höll <mhoell@student.tugraz.at>
2014-06-09 22:45:42 +0200

    [MH, FT]{other} - added WaterMonster Interface

---
+++ core/src/com/reu/game/monster/interfaces/WaterMonster.java
@@ -0,0 +1,15 @@
+package com.reu.game.monster.interfaces;
+
+public interface WaterMonster {
+
+	public static final short fire_weakness_ = 2;
+	public static final short water_weakness = 0;
+	public static final short electro_weakness = 10;
+
+	abstract void WaterAttack1();
+	abstract void WaterAttack2();
+	abstract void WaterAttack3();
+	abstract void WaterAttack4();
+
+
+}
