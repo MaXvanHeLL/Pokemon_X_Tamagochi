cdc2befd9fde750babcd6d9f1707d66c3ca675d8
Martin Skargeth <martin@skargeth.at>
2014-06-14 18:21:21 +0200

    [MS,MH]{other} - Finished kitchen, left debug button inside, don't forget do delete before presentation

--- core/src/com/reu/game/stages/Kitchen.java
+++ core/src/com/reu/game/stages/Kitchen.java
@@ -21,6 +21,8 @@ public class Kitchen extends ReuGameStage{
 	ReuGame parent_;
 	public static RoomType type_ = RoomType.KITCHEN;
 	private Table table_;
+	private Table stack_table_;
+	private Stack bar_stack_;
 
 	private ImageButton apple_button_;
 	private ImageButton carrot_button_;
@@ -39,37 +41,63 @@ public class Kitchen extends ReuGameStage{
 		table_.align(Align.top | Align.center);
 		
 		createButtons();
+		createStackTable();
+		buildTable();
 		
-		Table stackTable = new Table();
-		stackTable.align(Align.top | Align.center);
+		addActor(table_);
 		
+		// Create monster
+		monster_ = MonsterFactory.CreateMonster(type_, parent.getMonsterType());
+		addActor(monster_);
 		
-		stackTable.add().minHeight(Utils.GetPixelY(10)).width(Utils.GetPixelX(90)).colspan(3);
-		stackTable.row();
-		stackTable.add().width(Utils.GetPixelX(15));
-		
-		
-		Stack bar_stack = new Stack();
-		bar_stack.setWidth(Utils.GetPixelX(60f));
-		bar_stack.setHeight(Utils.GetPixelY(5f));
-		Table dirty_table = new Table();
-		dirty_table.setFillParent(true);
-		dirty_table.add(new Image(Utils.getAttentionColor(parent_.getNusselts_stats_().getHunger()).getDrawable("color"))).width(Utils.GetPixelX(60 * parent_.getNusselts_stats_().getHunger() / 100.f)).height(Utils.GetPixelY(5f)).left();
-		dirty_table.add().width(Utils.GetPixelX(60 * (1 - parent_.getNusselts_stats_().getHunger() / 100.f)));
-		bar_stack.add(dirty_table);
-		bar_stack.add(new Image(parent_.getSkin().getDrawable("BarFrame")));
-		
-		
-		stackTable.add(bar_stack).width(Utils.GetPixelX(60)).height(Utils.GetPixelY(5)); // Will be stack
-		
-		stackTable.add().width(Utils.GetPixelX(15));
-		
-		table_.add(stackTable).height(Utils.GetPixelY(135)).width(Utils.GetPixelX(90)).colspan(3);
+		// -- important for catching the Back Button to avoid program Exit !!
+		Gdx.input.setCatchBackKey(true);			
+	}
+	
+	private void buildTable()
+	{
+		table_.clearChildren();
+		table_.add(stack_table_).height(Utils.GetPixelY(135)).width(Utils.GetPixelX(90)).colspan(3);
 		table_.row();
-		/*//-------------------------------------------------------------------------------------------------------------------------
+				
+		table_.add(apple_button_).width(Utils.GetPixelX(20)).height(Utils.GetPixelY(20)).padLeft(Utils.GetPixelX(10)).padRight(Utils.GetPixelX(5)).padBottom(Utils.GetPixelY(5));
+		table_.add(carrot_button_).width(Utils.GetPixelX(20)).height(Utils.GetPixelY(20)).padLeft(Utils.GetPixelX(5)).padRight(Utils.GetPixelX(5)).padBottom(Utils.GetPixelY(5));
+		table_.add(pizza_button_).width(Utils.GetPixelX(20)).height(Utils.GetPixelY(20)).padLeft(Utils.GetPixelX(5)).padRight(Utils.GetPixelX(10)).padBottom(Utils.GetPixelY(5));
+	}
+	
+	private void createStackTable()
+	{
+		stack_table_ = new Table();
+		stack_table_.align(Align.top | Align.center);
+		
+		
+		stack_table_.add().minHeight(Utils.GetPixelY(10)).width(Utils.GetPixelX(90)).colspan(3);
+		stack_table_.row();
+		stack_table_.add().width(Utils.GetPixelX(15));
+		
+		
+		bar_stack_ = new Stack();
+		bar_stack_.setWidth(Utils.GetPixelX(60f));
+		bar_stack_.setHeight(Utils.GetPixelY(5f));
+		bar_stack_.add(new Image(parent_.getSkin().getDrawable("red")));
+		Table bar_table = new Table();
+		bar_table.setFillParent(true);
+		bar_table.add(new Image(parent_.getSkin().getDrawable("green"))).width(Utils.GetPixelX(60 * parent_.getNusselts_stats_().getHunger() / 100.f)).height(Utils.GetPixelY(5f)).left();
+		bar_table.add().width(Utils.GetPixelX(60 * (1 - parent_.getNusselts_stats_().getHunger() / 100.f)));
+		bar_stack_.add(bar_table);
+		bar_stack_.add(new Image(parent_.getSkin().getDrawable("BarFrame")));
+		
+		
+		stack_table_.add(bar_stack_).width(Utils.GetPixelX(60)).height(Utils.GetPixelY(5)); // Will be stack
+		
+		stack_table_.add().width(Utils.GetPixelX(15));
+		
+		
+		//-------------------------------------------------------------------------------------------------------------------------
 		// Test button which resets hunger to 0
 				
-				
+		stack_table_.row();
+		
 		ImageButtonStyle test_style = new ImageButtonStyle();
 		test_style.up = parent_.getSkin().getDrawable("kitchen_button_up");
 		test_style.down = parent_.getSkin().getDrawable("kitchen_button_down");
@@ -81,27 +109,16 @@ public class Kitchen extends ReuGameStage{
 			public void clicked(InputEvent event, float x, float y) 
 			{
 				parent_.getNusselts_stats_().setHunger(0);
+				createStackTable();
+	        	buildTable();
 			};
 		});
+		stack_table_.add();
+		stack_table_.add(test_button).center().width(Utils.GetPixelX(20)).height(Utils.GetPixelY(20)).padTop(Utils.GetPixelY(10));
 				
-		table_.add(test_button).bottom().left().width(Utils.GetPixelX(20)).height(Utils.GetPixelY(20)).padLeft(Utils.GetPixelX(5)).padTop(Utils.GetPixelY(5));
-				
-		table_.row().height(Utils.GetPixelY(100));
-		//-------------------------------------------------------------------------------------------------------------------------*/
+
+		//-------------------------------------------------------------------------------------------------------------------------
 		
-		
-		table_.add(apple_button_).width(Utils.GetPixelX(20)).height(Utils.GetPixelY(20)).padLeft(Utils.GetPixelX(10)).padRight(Utils.GetPixelX(5)).padBottom(Utils.GetPixelY(5));
-		table_.add(carrot_button_).width(Utils.GetPixelX(20)).height(Utils.GetPixelY(20)).padLeft(Utils.GetPixelX(5)).padRight(Utils.GetPixelX(5)).padBottom(Utils.GetPixelY(5));
-		table_.add(pizza_button_).width(Utils.GetPixelX(20)).height(Utils.GetPixelY(20)).padLeft(Utils.GetPixelX(5)).padRight(Utils.GetPixelX(10)).padBottom(Utils.GetPixelY(5));
-		
-		addActor(table_);
-		
-		// Create monster
-		monster_ = MonsterFactory.CreateMonster(type_, parent.getMonsterType());
-		addActor(monster_);
-		
-		// -- important for catching the Back Button to avoid program Exit !!
-		Gdx.input.setCatchBackKey(true);			
 	}
 	
 	private void createButtons()
@@ -137,6 +154,8 @@ public class Kitchen extends ReuGameStage{
 		        	parent_.getNusselts_stats_().setHunger(parent_.getNusselts_stats_().getHunger() + 10);
 		        	parent_.getNusselts_stats_().setWeight(parent_.getNusselts_stats_().getWeight() + 0.01f);
 		        	temp.eatSomething();
+		        	createStackTable();
+		        	buildTable();
 		        }
 		        else
 		        {
@@ -156,6 +175,8 @@ public class Kitchen extends ReuGameStage{
 		        	System.out.println(parent_.getNusselts_stats_().getHunger());
 		        	parent_.getNusselts_stats_().setHunger(parent_.getNusselts_stats_().getHunger() + 5);
 		        	temp.eatSomething();
+		        	createStackTable();
+		        	buildTable();
 		        }
 		        else
 		        {
@@ -176,6 +197,8 @@ public class Kitchen extends ReuGameStage{
 		        	parent_.getNusselts_stats_().setHunger(parent_.getNusselts_stats_().getHunger() + 20);
 		        	parent_.getNusselts_stats_().setWeight(parent_.getNusselts_stats_().getWeight() + 0.1f);
 		        	temp.eatSomething();
+		        	createStackTable();
+		        	buildTable();
 		        }
 		        else
 		        {
